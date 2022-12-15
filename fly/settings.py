1034ee37d5f65b1da55ebca78ba80b565c5120e8
Walison Filipe <walisonfilipe@hotmail.com>
2022-12-14 22:01:14 -0300

    Remove whitenoise e serve arquivos direto do fly

--- fly/settings.py
+++ fly/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,7 +120,6 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "public"
 STATIC_URL = "static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
